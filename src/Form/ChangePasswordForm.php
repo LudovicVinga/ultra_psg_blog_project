d2bb4188c40b4134ed9e555eaf88b6e4218ece24
Ludovic V <vinga.ludovic@gmail.com>
2025-06-02 10:45:30 +0200

    feat: password reset system implemented

---
+++ src/Form/ChangePasswordForm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class ChangePasswordForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez renseigner un mot de passe',
+                        ]),
+                        new Length([
+                            'min' => 12,
+                            'minMessage' => 'Le mot de passe doit contenir au minimum {{ limit }} caractères',
+                            'max' => 255,
+                            'maxMessage' => 'Le mot de passe ne doit pas dépasser {{ limit }} caractères',
+                        ]),
+                        new Regex([
+                            'pattern' => '/^(?=.*[a-zà-ÿ])(?=.*[A-ZÀ-Ỳ])(?=.*[0-9])(?=.*[^a-zà-ÿA-ZÀ-Ỳ0-9]).{11,255}$/',
+                            'match' => true,
+                            'message' => 'Le mot de passe doit contenir au moins une lettre miniscule, majuscule, un chiffre et un caractère spécial.',
+                        ]),
+                    ],
+                ],
+                'invalid_message' => 'Le nouveau mot de passe doit être identique a sa confirmation.',
+                // Instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
