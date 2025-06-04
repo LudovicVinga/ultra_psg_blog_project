ee0ff62cdee0712e303691ec474642ec52959e45
Ludovic V <vinga.ludovic@gmail.com>
2025-06-04 23:26:54 +0200

    feat: posts creation implemented

---
+++ src/Services/ApiService.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Services;
+
+class ApiService
+{
+    private string $uri = 'https://api.football-data.org/v4/competitions/FL1/teams';
+
+    private string $apiKey;
+
+    private mixed $matches;
+
+    public function __construct()
+    {
+        $this->loadApiKey();
+        $this->loadMatches();
+    }
+
+    public function loadApiKey(): void
+    {
+        $this->apiKey = $_ENV['FOOTBALL_API_KEY'];
+    }
+
+    public function loadMatches(): void
+    {
+        $uri = $this->uri;
+
+        $reqPrefs['http']['method'] = 'GET';
+        $reqPrefs['http']['header'] = "X-Auth-Token: {$this->apiKey}";
+        $stream_context = stream_context_create($reqPrefs);
+        $response = file_get_contents($uri, false, $stream_context);
+        $this->matches = json_decode($response);
+    }
+
+    public function getMatches(): mixed
+    {
+        return $this->matches;
+    }
+
+    public function getSquad(): mixed
+    {
+        return $this->matches->teams[8]->squad;
+    }
+
+    public function getCoach(): object
+    {
+        return $this->matches->teams[8]->coach;
+    }
+
+    public function getApiKey(): string
+    {
+        return $this->apiKey;
+    }
+
+
+    public function excludePlayersOfSquad(array $playersListToDelete = []): mixed
+    {
+        $playersListToShow = [];
+
+        foreach ($this->getSquad() as $player) {
+            if (!in_array($player->name, $playersListToDelete)) {
+                $playersListToShow[] = $player;
+            }
+        }
+
+        return $playersListToShow;
+    }
+}
