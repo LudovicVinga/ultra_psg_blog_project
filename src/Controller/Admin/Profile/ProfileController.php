2f88880e0bea53057981ef7452ba2ca91736f350
Ludovic V <vinga.ludovic@gmail.com>
2025-06-17 23:02:36 +0200

    feat: admin password edit implemented

--- src/Controller/Admin/Profile/ProfileController.php
+++ src/Controller/Admin/Profile/ProfileController.php
@@ -3,14 +3,14 @@
 namespace App\Controller\Admin\Profile;
 
 use App\Entity\User;
+use App\Form\EditPasswordFormType;
 use App\Form\EditProfileFormType;
-use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 #[Route('/admin')]
 final class ProfileController extends AbstractController
@@ -19,10 +19,8 @@ final class ProfileController extends AbstractController
     public function index(): Response
     {
         return $this->render('pages/admin/profile/index.html.twig', [
-
         ]);
     }
-
 
     #[Route('/profile/edit-profile', name: 'app_admin_profile_edit_profile', methods: ['GET', 'POST'])]
     public function editProfile(EntityManagerInterface $entityManager, Request $request): Response
@@ -35,20 +33,51 @@ final class ProfileController extends AbstractController
         $form = $this->createForm(EditProfileFormType::class, $admin);
         $form->handleRequest($request);
 
-        if( $form->isSubmitted() && $form->isValid())
-        {
-            $admin->setUpdatedAt(new DateTimeImmutable());
+        if ($form->isSubmitted() && $form->isValid()) {
+            $admin->setUpdatedAt(new \DateTimeImmutable());
 
             $entityManager->persist($admin);
             $entityManager->flush();
 
-            $this->addFlash('success', "Le profil a bien été mis à jour.");
+            $this->addFlash('success', 'Le profil a bien été mis à jour.');
 
             return $this->redirectToRoute('app_admin_profile_index');
         }
 
         return $this->render('pages/admin/profile/edit_profile.html.twig', [
-            "form" => $form->createView()
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/profile/edit-password', name: 'app_admin_profile_edit_password', methods: ['GET', 'POST'])]
+    public function editPassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $hasher): Response
+    {
+        $form = $this->createForm(EditPasswordFormType::class, null);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @var User
+             */
+            $admin = $this->getUser(); // On récupère l'admin uniquement lorsque les données auront été validées
+
+            $data = $form->getData();
+
+            $passwordHashed = $hasher->hashPassword($admin, $data['plainPassword']);
+
+            $admin->setPassword($passwordHashed);
+            $admin->setUpdatedAt(new \DateTimeImmutable());
+
+            $entityManager->persist($admin);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le mot de passe a bien été modifié.');
+
+            return $this->redirectToRoute('app_admin_profile_index');
+        }
+
+        return $this->render('pages/admin/profile/edit_password.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
