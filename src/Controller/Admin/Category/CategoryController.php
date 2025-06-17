fa2c76c8244c19ac2cc8c80b9c086f975087a0fa
Ludovic V <vinga.ludovic@gmail.com>
2025-06-17 11:36:55 +0200

    testons

--- src/Controller/Admin/Category/CategoryController.php
+++ src/Controller/Admin/Category/CategoryController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 #[Route('/admin')]
 final class CategoryController extends AbstractController
 {
